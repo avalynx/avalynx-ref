16d6c35832a9e4f58eef79e6fa8ddc3dffb51ab1
jschwind <info@juergen-schwind.de>
2024-05-27 18:01:19 +0200

    log removed

--- src/js/avalynx-ref.esm.js
+++ src/js/avalynx-ref.esm.js
@@ -24,7 +24,6 @@ export class AvalynxRef {
             selector = '.' + selector;
         }
         this.elements = document.querySelectorAll(selector);
-        console.log('this.elements count:', this.elements.length);
         this.options = {
             isHtml: false,
             ...options
